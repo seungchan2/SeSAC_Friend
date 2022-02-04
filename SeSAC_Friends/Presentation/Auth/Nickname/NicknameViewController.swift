898b87bc8c2d40dcd966d172344c9eab25800c55
chansenbbai <chanchu@hansung.ac.kr>
2022-02-04 19:48:04 +0900

    [#29] 닉네임, 이메일뷰 UI Code로 수정
    ### Description
    
    - 닉네임, 이메일 뷰 UI Code로 수정
    - 세 개의 뷰가 UI가 똑같고 text만 바껴서 Common으로 빼고 수정
    
    Resolve #29

--- SeSAC_Friends/Presentation/Auth/Nickname/NicknameViewController.swift
+++ SeSAC_Friends/Presentation/Auth/Nickname/NicknameViewController.swift
@@ -9,8 +9,26 @@ import UIKit
 
 final class NicknameViewController: BaseViewController {
 
+    let nicknameView = AuthCommonView()
+    
+    override func loadView() {
+        view = nicknameView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    override func style() {
+        super.style()
+        view.backgroundColor = Color.sesacWhite
+        setNicknameView()
+    }
+    
+    private func setNicknameView() {
+        nicknameView.explainLabel.text = "닉네임을 입력해주세요"
+        nicknameView.commonTextField.textField.placeholder = "10자 이내로 입력"
+    }
+    
 }
