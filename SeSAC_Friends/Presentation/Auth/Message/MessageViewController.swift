efbca36990b51ac3bed9c9c0999a778d5257bcba
chansenbbai <chanchu@hansung.ac.kr>
2022-02-05 18:09:19 +0900

    [#37] Message 뷰 로직 추가
    ### Description
    
    - 타이머 로직 추가
    
    Resolve #37

--- SeSAC_Friends/Presentation/Auth/Message/MessageViewController.swift
+++ SeSAC_Friends/Presentation/Auth/Message/MessageViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 final class MessageViewController: BaseViewController {
    
     let messageView = MessageView()
+    var timer: DispatchSourceTimer?
+    var remainingTime = 300
     var auth = Auth.shared
     
     override func loadView() {
@@ -18,11 +20,57 @@ final class MessageViewController: BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(auth.phoneNumber)
+        setAddTarget()
     }
-    
+     
     override func style() {
         super.style()
         view.backgroundColor = Color.sesacWhite
     }
+    
+    private func setAddTarget() {
+        messageView.timerButton.addTarget(self, action: #selector(touchTimerButton), for: .touchUpInside)
+        messageView.confirmButton.addTarget(self, action: #selector(touchConfirmButton), for: .touchUpInside)
+        messageView.backButton.addTarget(self, action: #selector(touchBackButton), for: .touchUpInside)
+    }
+    
+    @objc func touchTimerButton() {
+        startTimer()
+     }
+    
+    @objc func touchConfirmButton() {
+        navigationController?.pushViewController(NicknameViewController.instanceFromNib(), animated: true)
+     }
+    
+    @objc func touchBackButton() {
+        navigationController?.popViewController(animated: true)
+     }
+    
+    private func startTimer() {
+           if self.timer == nil {
+               self.timer = DispatchSource.makeTimerSource(flags: [], queue: .main)
+               self.timer?.schedule(deadline: .now(), repeating: 1)
+               self.timer?.setEventHandler(handler: { [weak self] in
+                   guard let self = self else { return }
+                   self.remainingTime -= 1
+                   let min = (self.remainingTime % 3600) / 60
+                   let sec = (self.remainingTime % 3600) % 60
+                   self.messageView.timerLabel.text = String(format: "%02d:%02d", min,sec)
+                   if self.remainingTime <= 0 {
+                       self.stopTimer()
+                   }
+               })
+               self.timer?.resume()
+           } else {
+               stopTimer()
+               remainingTime = 300
+               startTimer()
+           }
+       }
+       
+    private func stopTimer() {
+           self.timer?.cancel()
+           self.timer = nil
+       }
+    
 }
